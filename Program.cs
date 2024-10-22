70ccabb79caa84c640fdc08c0554cdfcfda56a6e
Anatolius <Anatolius@DESKTOP-8I30QGB>
2024-10-22 17:31:21 +0200

    u

--- Program.cs
+++ Program.cs
@@ -20,26 +20,35 @@
             //Console.WriteLine("Liste aller Eingabegeräte:");
             //Info.ListAllDevices(adbPath, logFilePath);
             //Console.WriteLine("Starte die Erfassung von Touch-Ereignissen...");
-            
             //Makro.TrackTouchEvents(adbPath, inputDevice, "C:\\Users\\Anatolius\\Source\\Repos\\I-Danilov-I\\EMU\\TouchLogs.txt");
 
 
             ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "00000017", "000002b0"); // Öffne Seitenmenü
             Thread.Sleep(5000);
-
             ClicksOperate.ClickAndHoldAndScroll(adbPath, "0000005b", "000003ab", "00000025", "000000b5", 300, 500); // Switsche runter
+            Thread.Sleep(5000);
 
             Screenshot.TakeScreenshot(adbPath, screenshotDirectory);
             bool findOrNot = Screenshot.CheckTextInScreenshot(screenshotDirectory, "Belohnung");
-            if (findOrNot) 
+            Thread.Sleep(5000);
+            if (findOrNot)
             {
                 ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "0000003b", "000003f8"); // Wähle BEgleiter Abenteuer
                 Thread.Sleep(5000);
 
                 ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "000001c3", "000002ce");
+                Thread.Sleep(5000);
                 ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "000001c3", "000002ce");
+                Thread.Sleep(5000);
                 ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "000001c3", "000002ce");
+                Thread.Sleep(5000);
                 ClicksOperate.ClickAtTouchPositionWithHexa(adbPath, "000001b5", "0000049c");
+                Thread.Sleep(5000);
+            }
+            else 
+            {
+                Console.WriteLine("Keine Online Belohnung verfügbar, veersuche später erneut.");
+                Thread.Sleep(5000);
             }
         }
     }
