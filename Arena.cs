605361dd20988f68f48f57c3965ab66f3a7ef1aa
Anatolius <Anatolius@DESKTOP-8I30QGB>
2024-10-27 09:04:46 +0100

    z

--- Arena.cs
+++ Arena.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace EMU
+﻿namespace EMU
 {
     internal class Arena(WriteLogs writeLogs ,DeviceControl deviceControl)
     {
@@ -10,9 +8,17 @@ namespace EMU
             deviceControl.ClickAtTouchPositionWithHexa("000001bc", "000003a8"); // Kraft
             deviceControl.ClickAtTouchPositionWithHexa("000002f4", "000002ce"); // Truppenstäerke
             deviceControl.ClickAtTouchPositionWithHexa("000002e6", "000004ba"); // Latenzträger
-            deviceControl.ClickAndHoldAndScroll("000002bd", "000004bc", " 00000027", "000000da", 300, 500);
-            deviceControl.ClickAtTouchPositionWithHexa("0000027f", "0000018b"); // Dock
-            Thread.Sleep(4000);
+            deviceControl.ClickAtTouchPositionWithHexa("00000286", "000001ff"); // Scharfschützen
+            deviceControl.ClickAtTouchPositionWithHexa("00000347", " 000003b7"); // Arena
+            deviceControl.ClickAtTouchPositionWithHexa("000001f6", "000005e9"); // Herausfordern
+
+
+
+            deviceControl.ClickAtTouchPositionWithHexa("00000313", "00000190"); // Den erten von oben anklciken
+            deviceControl.ClickAtTouchPositionWithHexa("000000ef", "000005d8"); // Schnelleinsatz
+            deviceControl.ClickAtTouchPositionWithHexa("00000287", "000005d6"); // Kampf
+            Thread.Sleep(45 * 1000);
+            deviceControl.BackUneversal();
         }
     }
 }
